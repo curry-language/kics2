bc0bd75627f1f2811e0295ce4bc383a33b593fc2
Fredrik Wieczerkowski <fredrik.wieczerkowski@stu.uni-kiel.de>
2021-06-07 15:12:01 +0200

    Update distribution tarball

--- mk/dist.mk
+++ mk/dist.mk
@@ -4,7 +4,9 @@ DISTNAME = kics2-$(VERSION)
 TARBALLNAME = $(DISTNAME).tar.gz
 
 DISTDIR = $(DISTROOTDIR)/$(DISTNAME)
+DISTSRCDIR = $(DISTDIR)/src
 DISTBINDIR = $(DISTDIR)/bin
+DISTLIBDIR = $(DISTDIR)/lib
 DISTLOCALBINDIR = $(DISTBINDIR)/.local
 export TARBALL = $(DISTROOTDIR)/$(TARBALLNAME)
 
@@ -25,13 +27,24 @@ export DIST_ARTIFACTS = $(DISTROOTDIR)
 # take advantage the fully bootstrapped `kics2c`
 # to quickly `make` the complete KiCS2 system.
 
-$(TARBALL): $(DISTBINDIR)/kics2-frontend $(DISTLOCALBINDIR)/kics2c $(DISTDIR)
-	rm -rf $(DISTDIR)/.git $(DISTDIR)/**/.git
+$(TARBALL): $(DISTBINDIR)/kics2-frontend $(DISTLOCALBINDIR)/kics2c $(DISTSRCDIR)/Installation.curry $(DISTLIBDIR)/VERSION $(DISTDIR)
+	# Make sure that `make` doesn't have to rebuild `kics2c`
+	touch $(DISTLIBDIR)/VERSION
+	touch $(DISTSRCDIR)/Installation.curry
+	touch $(DISTLOCALBINDIR)/kics2c
+
+	# Create the tarball
 	cd $(DISTROOTDIR) && tar -cvzf $(TARBALLNAME) $(DISTNAME)
 
 $(DISTDIR): | $(DISTROOTDIR)
 	rm -rf $(DISTDIR)
-	git clone $(ROOT) $(DISTDIR) --recurse-submodules
+	git clone $(ROOT) $(DISTDIR)
+	cat $(ROOT)/.dist-modules | sed 's|ROOT|$(ROOT)|' > $(DISTDIR)/.gitmodules
+	cd $(DISTDIR) && git submodule update --init
+	rm -rf $(DISTDIR)/.git $(DISTDIR)/**/.git $(DISTDIR)/.gitmodules $(DISTDIR)/.dist-modules
+
+$(DISTSRCDIR)/%: $(SRCDIR)/%
+	cp $< $@
 
 $(DISTBINDIR)/%: $(BINDIR)/% | $(DISTBINDIR)
 	cp $< $@
@@ -39,8 +52,14 @@ $(DISTBINDIR)/%: $(BINDIR)/% | $(DISTBINDIR)
 $(DISTLOCALBINDIR)/%: $(LOCALBINDIR)/% | $(DISTLOCALBINDIR)
 	cp $< $@
 
+$(DISTLIBDIR)/%: $(LIBDIR)/% | $(DISTLIBDIR)
+	cp $< $@
+
 $(DISTBINDIR): | $(DISTDIR)
 	mkdir -p $@
 
 $(DISTLOCALBINDIR): | $(DISTDIR)
+	mkdir -p $@
+
+$(DISTLIBDIR): | $(LIBDIR)
 	mkdir -p $@
