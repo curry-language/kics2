7048bd4bdc681262a05890661b2a94c9f47b3fae
Fredrik Wieczerkowski <fredrik.wieczerkowski@stu.uni-kiel.de>
2021-10-08 00:49:24 +0200

    Place utils in bin/.local
    In order to avoid polluting the global PATH, if the user has added the
    bin directory to it.

--- mk/utils.mk
+++ mk/utils.mk
@@ -1,14 +1,14 @@
 # Makefile for building installation utils
 
 # The cleancurry binary
-export CLEANCURRY = $(BINDIR)/cleancurry
+export CLEANCURRY = $(LOCALBINDIR)/cleancurry
 
-export UTILS = $(foreach u, pwd cleancurry which, $(BINDIR)/$(u)$(EXE_SUFFIX))
+export UTILS = $(foreach u, pwd cleancurry which, $(LOCALBINDIR)/$(u)$(EXE_SUFFIX))
 export UTILS_ARTIFACTS = $(UTILSDIR)/*.hi \
                          $(UTILSDIR)/*.o
 
 .PHONY: all
 all: $(UTILS)
 
-$(UTILS): $(BINDIR)/%$(EXE_SUFFIX): $(UTILSDIR)/%.hs | $(BINDIR) $(STACKYAML)
+$(UTILS): $(LOCALBINDIR)/%$(EXE_SUFFIX): $(UTILSDIR)/%.hs | $(LOCALBINDIR) $(STACKYAML)
 	$(GHC) --make -Wall -O2 -o $@ $<
