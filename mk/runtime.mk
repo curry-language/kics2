d45fe792b23a903aa723719d6871b8b7723ced5b
Fredrik Wieczerkowski <fredrik.wieczerkowski@stu.uni-kiel.de>
2022-07-04 16:52:54 +0200

    Remove some unused runtime options

--- mk/runtime.mk
+++ mk/runtime.mk
@@ -8,16 +8,6 @@ RUNTIME_CABAL = $(RUNTIMEDIR)/$(RUNTIME_PACKAGE).cabal
 RUNTIME_CABAL_IN = $(RUNTIME_CABAL).in
 # IDSupply implementation to use (see kics2-runtime.cabal for a list)
 export RUNTIME_IDSUPPLY = idsupplyinteger
-# Additional flags for compilation of the runtime
-RUNTIME_FLAGS =
-
-# the runtime package configuration file
-RUNTIME_PKG_CONF = $(LOCALPKG)/runtime.conf
-
-# runtime dependencies as comma separated list
-RUNTIME_CABAL_DEPS  = $(call comma_sep,$(RUNTIMEDEPS))
-# GHC options passed to the cabal file
-RUNTIME_GHC_OPTS = $(RUNTIME_FLAGS:%=-D%)
 
 export RUNTIME = $(RUNTIME_CABAL)
 export RUNTIME_ARTIFACTS = $(RUNTIMEDIR)/dist \
