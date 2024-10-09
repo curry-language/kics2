815fb9dacab26011d5bb434c819f3bf47269f643
Michael Hanus <mh@informatik.uni-kiel.de>
2024-10-09 19:56:50 +0200

    Go back to the deterministic REPL version since ND problem with ReadShowTerm has been fixed

--- boot/REPLBoot.hs
+++ boot/REPLBoot.hs
@@ -4,12 +4,5 @@ module Main where
 import Basics
 import KiCS2.Curry_REPL
 
--- TODO: Make the REPL fully deterministic.
---       One source of nondeterminism seems to be ReadShowTerm
---       (invoked from AbstractCurry.Files), whose functions
---       apparently when invoked always get classified as being
---       nondeterministic (due to the Data dictionary?)
-
 main :: IO ()
-main = evalIO nd_C_main
--- main = evalDIO d_C_main
+main = evalDIO d_C_main
