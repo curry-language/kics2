9e77e235c2b4f7e01a894104926e8d40eb3489aa
Michael Hanus <mh@informatik.uni-kiel.de>
2025-01-30 15:07:13 +0100

    Precompile libraries also with `--comments` and `--ast`

--- scripts/compile-all-libs.sh
+++ scripts/compile-all-libs.sh
@@ -4,25 +4,27 @@
 CURRY=kics2
 FRONTEND=bin/$CURRY-frontend
 FRONTENDPARAMS="-o .curry/kics2-$VERSION -D__KICS2__=$MAJORVERSION$(printf "%02d
-" $MINORVERSION) --extended -Ono-remove-unused-imports -ilib AllLibraries"
+" $MINORVERSION) -Ono-remove-unused-imports -ilib AllLibraries"
 
 compile_all() {
-  $FRONTEND --flat                $FRONTENDPARAMS
-  $FRONTEND --type-annotated-flat $FRONTENDPARAMS
-  $FRONTEND --acy                 $FRONTENDPARAMS
-  $FRONTEND --uacy                $FRONTENDPARAMS
+  "$FRONTEND" --flat                       $FRONTENDPARAMS
+  "$FRONTEND" --type-annotated-flat --flat $FRONTENDPARAMS
+  "$FRONTEND" --acy                        $FRONTENDPARAMS
+  "$FRONTEND" --uacy                       $FRONTENDPARAMS
+  "$FRONTEND" --comments                   $FRONTENDPARAMS
+  "$FRONTEND" --ast                        $FRONTENDPARAMS
 }
 
 TMPOUT=TMPLIBOUT
 CCODE=0
 
 while [ $CCODE = 0 ] ; do
-  compile_all | tee $TMPOUT
+  compile_all | tee "$TMPOUT"
   echo NEW COMPILED LIBRARIES IN THIS ITERATION:
-  grep Compiling $TMPOUT
+  grep Compiling "$TMPOUT"
   CCODE=$?
 done
-/bin/rm -r $TMPOUT
+/bin/rm -r "$TMPOUT"
 
 echo "Compile Haskell targets..."
 bin/$CURRY --nocypm :set parser -Ono-remove-unused-imports :l AllLibraries :eval "42::Int" :quit
